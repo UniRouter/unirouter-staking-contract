ccc69aa262a05c63aa1f1ee1c06dad2da5aa0f76
scoopx <hx364@nyu.edu>
2024-04-12 23:06:30 +0800

    build

--- src/URORewardPoints.sol
+++ src/URORewardPoints.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.9.6/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // interface IERC20 {
 //     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
